fe2289ccc9323d15ab2e613ff9d5e32a60d2eb36
Cristian David Vargas <cristiandavidvargas2@icloud.com>
2024-07-28 15:29:48 -0500

    Fixed Email Sender Trouble

--- model/utils/emailService.js
+++ model/utils/emailService.js
@@ -1,8 +1,9 @@
 import nodemailer from 'nodemailer'
-import fs from 'fs'
+import fs from 'fs/promises' // Usando fs/promises en lugar de fs
 import path from 'path'
 import { fileURLToPath } from 'url'
 import 'dotenv/config'
+
 const smtp = nodemailer.createTransport({
   service: 'gmail',
   port: 465,
@@ -10,7 +11,6 @@ const smtp = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD
-
   }
 })
 
@@ -23,14 +23,13 @@ export default class EmailService {
   static async sendEmailVerify (input) {
     const { email, verificationToken } = input
     try {
-      let html = fs.readFileSync(templatePath, 'utf8')
-      html = html.replace('{{url}}', `https://mercurial-app.vercel.app/users/verify/user/${verificationToken}`).replace('{{text}}', 'Verificar cuenta').replace('{{body}}', 'Haz clic en el siguiente botón para verificar tu cuenta:')
-      await smtp.sendMail({
-        from: `Mercurial Team <${process.env.EMAIL}>`,
-        to: email,
-        subject: 'Welcome to Mercurial App, please verify your email',
-        html
+      let html = await loadHtmlFile(templatePath)
+      html = replaceVariables(html, {
+        url: `https://mercurial-app.vercel.app/users/verify/user/${verificationToken}`,
+        text: 'Verificar cuenta',
+        body: 'Haz clic en el siguiente botón para verificar tu cuenta:'
       })
+      await sendEmail(html, 'Verify your account', email)
       return true
     } catch (e) {
       throw new Error(e)
@@ -40,15 +39,13 @@ export default class EmailService {
   static async sendEmailResetPassword (input) {
     const { email, token } = input
     try {
-      let html = fs.readFileSync(templatePath, 'utf8')
-      html = html.replace('{{url}}', `https://mercurial-app.vercel.app/users/reset/password/${token}`).replace('{{text}}', 'Restablecer Contraseña').replace('{{body}}', 'Haz clic en el siguiente botón para restablecer tu contraseña:')
-
-      await smtp.sendMail({
-        from: `Mercurial Team <${process.env.EMAIL}>`,
-        to: email,
-        subject: 'Reset your password',
-        html
+      let html = await loadHtmlFile(templatePath)
+      html = replaceVariables(html, {
+        url: `https://mercurial-app.vercel.app/users/reset/password/${token}`,
+        text: 'Restablecer Contraseña',
+        body: 'Haz clic en el siguiente botón para restablecer tu contraseña:'
       })
+      await sendEmail(html, 'Reset your password', email)
       return true
     } catch (e) {
       throw new Error(e)
@@ -58,18 +55,51 @@ export default class EmailService {
   static async passwordChangeConfirmation (input) {
     const { email } = input
     try {
-      let html = fs.readFileSync(templatePath, 'utf8')
-      const url = 'https://mercurial-app.vercel.app/'
-      html = html.replace('{{url}}', url).replace('{{text}}', 'Iniciar Sesión').replace('{{body}}', 'Tu contraseña ha sido cambiada exitosamente.')
-      await smtp.sendMail({
-        from: `Mercurial Team <${process.env.EMAIL}>`,
-        to: email,
-        subject: 'Password changed',
-        html
+      let html = await loadHtmlFile(templatePath)
+      html = replaceVariables(html, {
+        url: 'https://mercurial-app.vercel.app/',
+        text: 'Iniciar Sesión',
+        body: 'Tu contraseña ha sido cambiada exitosamente.'
       })
+      await sendEmail(html, 'Password changed', email)
       return true
     } catch (e) {
       throw new Error(e)
     }
+  }
+}
+
+async function loadHtmlFile (filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8')
+    return data
+  } catch (error) {
+    console.error('Error loading HTML file:', error)
+    throw error
+  }
+}
+
+function replaceVariables (html, variables) {
+  let modifiedHtml = html
+  for (const [key, value] of Object.entries(variables)) {
+    const regex = new RegExp(`{{${key}}}`, 'g')
+    modifiedHtml = modifiedHtml.replace(regex, value)
+  }
+  return modifiedHtml
+}
+
+async function sendEmail (htmlContent, subject, email) {
+  const mailOptions = {
+    from: `Mercurial Team <${process.env.EMAIL}>`,
+    to: email,
+    subject,
+    html: htmlContent
+  }
+
+  try {
+    const info = await smtp.sendMail(mailOptions)
+    console.log('Email sent: ' + info.response)
+  } catch (error) {
+    console.error('Error sending email:', error)
   }
 }
